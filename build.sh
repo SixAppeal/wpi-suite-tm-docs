385adb9304bcda466ded0bd5c574c4cc3786287f
William Temple <wmtemple@wpi.edu>
2014-12-16 21:22:20 -0500

    Modified for footer and cleaned md files.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 for f in `find md -mindepth 1`; do
-	pandoc -o out/$(basename ${f} .md).html --include-in-header=res/header.html.inc $f;
+	echo Converting $f;
+	pandoc -o out/$(basename ${f} .md).html --include-in-header=res/header.html.inc -A res/footer.html.inc $f;
 done
